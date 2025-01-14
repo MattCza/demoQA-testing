8e44b943af06b1a2a7683f352d8c42d57c4ead05
Mateusz Czajkowski/Modem Verification Support II (SDV) /SRPOL/Professional/Samsung Electronics <m.czajkowsk2@samsung.com>
2025-01-14 15:21:48 +0100

    Validate that the displayed output matches the input provided

--- demoQA/src/test/java/com/demoqa/tests/elements/TextBoxTest.java
+++ demoQA/src/test/java/com/demoqa/tests/elements/TextBoxTest.java
@@ -10,19 +10,34 @@ public class TextBoxTest extends BaseTest {
     @Test
     public void testTextBox() {
         String fullName = "Mark Griffith";
+        String email = "MG@gmail.com";
         String currentAddress = "Testing current address";
+        String permanentAddress = "This is permanent address";
 
         TextBoxPage textBoxPage = homePage.goToElementsCard().clickTextBoxItem();
         textBoxPage.setFullNameField(fullName);
-        textBoxPage.setEmailField("MG@gmail.com");
+        textBoxPage.setEmailField(email);
         textBoxPage.setCurrentAddressField(currentAddress);
-        textBoxPage.setPermanentAddressField("Now: permanent address");
+        textBoxPage.setPermanentAddressField(permanentAddress);
 
         textBoxPage.clickSubmitButton();
 
+        String actualFullName = textBoxPage.getNameResult().substring(5);
+        String actualEmail = textBoxPage.getEmailResult().substring(6);
         String actualCurrentAddress = textBoxPage.getCurrentAddressResult().substring(17);
+        String actualPermanentAddress = textBoxPage.getPermanentAddressResult().substring(20);
+
+
+        Assert.assertEquals(actualFullName, fullName,
+                "\n Actual Full Name field response is different form provided: " + fullName);
+
+        Assert.assertEquals(actualEmail, email,
+                "\n Actual Email response is different form provided: " + email);
 
         Assert.assertEquals(actualCurrentAddress, currentAddress,
-                "\n Actual response is different form: " + currentAddress);
+                "\n Actual Current Address response is different form provided: " + currentAddress);
+
+        Assert.assertEquals(actualPermanentAddress, permanentAddress,
+                "\n Actual Permanent Address response is different form provided: " + permanentAddress);
     }
 }
